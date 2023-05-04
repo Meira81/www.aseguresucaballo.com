9035ec7a5872c85ce53a80b03b470411b0f43131
Meira81 <guadalupe.alonso.meira@gmail.com>
2023-05-04 22:45:46 +0200

    cambios css

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -21,32 +21,13 @@ const Information = () => {
 
   return (
     <>
-      <figure className="information-image">
+      
+      {/* <div className="information-text-form"> */}
+        
+        <figure className="information-figure-image">
         <img className="information-image" src={image} alt="imagen-caballo" />
       </figure>
-      <div className="information-text-form">
-        <section className="info">
-          <h1>Asegure su caballo de recreo</h1>
-          <p>
-            Desde aseguresucaballo.com queremos ofrecerle el seguro de
-            responsabilidad civil para su caballo.
-          </p>
-          <p>
-            Con una cobertura anual de 300.000€ estará cubierto ante cualquier
-            incidencia.
-          </p>
-          <p>
-            Además la póliza incluye 300€ para los gastos de retirada de
-            cadáver.
-          </p>
-          <p>
-            Sin franquicia en daños corporales ni en la retirada del caballo en
-            caso de fallecimiento; solo en daños materiales se aplica una
-            franquicia de 75€.
-          </p>
-          <p>Se incluyen además 6.000€ para Fianzas y Defensa.</p>
-        </section>
-        <form onSubmit={handleSubmit}>
+       {/*  <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="name">Nombre completo</label>
             <input
@@ -70,8 +51,8 @@ const Information = () => {
             />
           </div>
           <button type="submit">Enviar</button>
-        </form>
-      </div>
+        </form> */}
+      {/* </div> */}
     </>
   );
 };
